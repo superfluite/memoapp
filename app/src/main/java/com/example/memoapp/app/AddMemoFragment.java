47554b15e58fb571b96f8b59f8c57fff6c7d4cd7
superfluite <superfluite_@naver.com>
2014-05-14 18:37:33 +0900

    addmemo fragment layout

---
+++ app/src/main/java/com/example/memoapp/app/AddMemoFragment.java
@@ -0,0 +1,24 @@
+package com.example.memoapp.app;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+
+public class AddMemoFragment extends Fragment {
+
+    private EditText memoText;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_add_memo_fragment,null,false);
+    }
+}
--- app/src/main/java/com/example/memoapp/app/MainActivity.java
+++ app/src/main/java/com/example/memoapp/app/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.memoapp.app;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,10 +10,22 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private enum Fragmentindex {
+        ADD(0);
+        private int value;
+        private Fragmentindex(int value){
+            this.value=value;
+        }
+    }
+    Fragmentindex currentFragmentIndex;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currentFragmentIndex=Fragmentindex.ADD;
+        changeFrag(currentFragmentIndex);
     }
 
 
@@ -35,4 +49,23 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void changeFrag(Fragmentindex index){
+        Fragment frag = null;
+        frag = getFrag(index);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frag_container,frag);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    public Fragment getFrag(Fragmentindex index){
+        Fragment frag = null;
+        switch (index) {
+            case ADD:
+                frag=new AddMemoFragment();
+            default:
+                break;
+        }
+        return frag;
+    }
 }
